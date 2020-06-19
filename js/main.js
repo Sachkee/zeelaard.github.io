56568f1fbbcb697381bd9cc536117f004c889572
zeelaard <zeelaard@gmail.com>
2020-06-19 21:37:59 +0200

    bottstrap

---
+++ js/main.js
@@ -0,0 +1,61 @@
+function orderButtonClick() {
+    const inputName = document.querySelector("input#name"); /* A név mező értékének beolvasása */
+    const inputEmail = document.querySelector("input#email"); /* Az email mező értékének beolvasása */
+    const inputAddress = document.querySelector("input#address"); /* Az cím mező értékének beolvasása */
+    const inputRemarks = document.querySelector("textarea#remarks"); /* Az megjegyzés mező értékének beolvasása */
+    const inputQuantity = document.querySelector("input#quantity"); /* A mennyiség értékének (szám) beolvasása */
+
+    const validResults = checkInputValidation(inputName.value, inputEmail.value, inputAddress.value, inputRemarks.value,
+        parseInt(inputQuantity.value));
+
+    if (validResults == true) {
+        let amount = calcOrderPrice(parseInt(inputQuantity.value));
+        showPriceOnPage(amount);
+    }
+}
+
+function checkInputValidation(name, email, address, remarks, qty) {
+    let validResults = false;
+    if (name.length < 6) {
+        alert("A név nincs beírva vagy kevesebb, mint 6 karakter!");
+    } else if (email.indexOf(".") == -1 || email.indexOf("@") == -1) {
+        alert("Az email nincs beírva, vagy helytelenül van megadva!");
+    } else if (address.length < 10) {
+        alert("A megadott cím nincs kitöltve vagy kevesebb, mint 10 karakter!");
+    } else if (remarks.indexOf("<") >= 0 || remarks.indexOf(">") >= 0) {
+        alert("A megjegyzés nem tartalmazhat html elemeket!");
+    } else if (isNaN(qty) || qty < 1 || qty > 10) {
+        alert("A mennyiség legalább 1, legfeljebb 10db lehet!");
+        inputQuantity.value = "1";
+    } else {
+        validResults = true;
+    }
+    return validResults;
+}
+
+function calcOrderPrice(qty) {
+    const basePrice = 1200; //Az alap hamburger árának meghatározása
+    const radioAddition = parseInt(document.querySelector(".form-check-input:checked").value); //A kiválasztott feltét értékének beolvasása
+    const selectSauce = parseInt(document.querySelector("select#sauce").value); //A kiválasztott szósz értékének beolvasása
+    return (basePrice + radioAddition + selectSauce) * qty; //Rendelés összegének kiszámoltatása    
+}
+
+function showPriceOnPage(amount) {
+    const showTotalPrice = document.querySelector("span.show-price"); //A span elem meghatározása a végösszeg kiíratásához
+    const showDeliveryCost = document.querySelector("span.delivery-cost"); //A span elem meghatározása a szállítási díj kiíratásához
+
+    if (amount < 5000) {
+        showTotalPrice.innerHTML = amount + 500; //Végösszeg megjelenítése
+        showDeliveryCost.innerHTML = "(500Ft szállítási díjat tartalmaz)" //Szállítási díj megjelenítése
+    } else {
+        showTotalPrice.innerHTML = amount;
+        showDeliveryCost.innerHTML = "(5000Ft felett nincs szállítási díj)"
+    }
+}
+
+//További lehetőségek a radio button értékének beolvasásához
+//const radioAddition = parseInt(document.querySelector("input.form-check-input:checked").value);
+//const radioAddition = parseInt(document.querySelector("input[name='radio-additions']:checked").value);
+//const radioAddition = parseInt(document.querySelector('input[name=radio-additions]:checked').value);
+//Másik lehetőség a végösszeg megjelenítéséhez:
+//showTotalPrice.innerHTML = '${amount}';
